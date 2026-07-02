359995668986057375342e54eddb8dcd76dc32f2
Zewdnehhaile <nehzewdd@gmail.com>
2026-07-02 11:38:41 +0300

    Fix __dirname issue and deploy notification API endpoints

--- server.ts
+++ server.ts
@@ -1,7 +1,5 @@
-import { fileURLToPath } from 'url';
 import { dirname } from 'path';
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const __dirname = import.meta.dirname;
 import 'dotenv/config';
 import express from "express";
 import path from "path";
